4858af93982876a51a61218b789c11fc127445ae
remah amr <remah3335@gmail.com>
2019-09-15 00:09:28 +0200

    initial author route

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const app = express()
 const expressHbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash');
+const session = require('express-session');
+
 
 const indexRoute = require('./routes/index')
+const authorsRoute = require('./routes/authors')
 
 app.engine('hbs',expressHbs({layoutsDir: 'views/layouts/',defaultLayout: 'main-layout',extname: 'hbs'})); // since hbs not built in express we have to register hbs engine to express TO USING IT
 app.set('view engine','hbs'); 
 app.set('views','views'); 
 
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+
+app.use(session({secret:'my secret',resave:false,saveUninitialized:false}));
+
+app.use(flash()); 
+
+app.use((req,res,next) => {
+  res.locals.errorMessage = req.flash('error');
+  res.locals.successMessage = req.flash('success');
+  next(); 
+})
+
 app.use(indexRoute)
+app.use('/authors',authorsRoute)
 
 mongoose.connect(keys.MONGO_URI,{ useNewUrlParser: true , useUnifiedTopology: true  }).then(()=>{ console.log('mongodb connected!');})
 
---
+++ routes/authors.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// all authors
+router.get('/',async (req,res) => {
+    let searchOptions = {} // empty object has fields samr of data in db to search by it when throwing in find method
+    if(req.query.name !== null && req.query.name !== '' ) // you can remove this condition but to arrange only if you don't enter any search so name is null or you enter search but the input is empty
+    {
+        searchOptions.name = new RegExp(req.query.name,'i') // i : search no case sensetive
+    }
+    try {
+        const authors = await Author.find()
+        res.render('authors/index',{
+            authors: authors
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+ 
+
+// get new author
+router.get('/new',(req,res)=>{
+    res.render('authors/new')
+})
+
+// post new author
+router.post('/',async (req,res)=>{
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try{
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    }catch{
+        res.render('authors/new',{
+            author:author,
+            errorMessage: 'Failed to create author!'
+        })
+        // req.flash('error','Failed to create author!')
+        // res.redirect('authors/index')
+        // above if you use req.flash it works after reload the page by redirect not render
+    }
+    // with callback function
+    // author.save((err,author) => {
+    //     if(err){
+            // res.render('authors/new',{
+            //     author: author
+            // })
+    //     }
+    //     else{
+    //         res.render('authors/index')
+    //     }
+    // })
+})
+
+module.exports = router
